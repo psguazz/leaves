4b93311a27a3e17ef9c7a6d57cad2df2280f6ff5
Sguazz <psguazza@bwc.org>
2014-10-03 20:35:21 +0300

    Connecting to the SW web service
    The back-end should be able to send requests to the Semantic web-service
    of choice. This sends a sample query and parses the results.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,35 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class SearchController < ApplicationController
   def fields
-    render json: [{ 'name' => 'First', 'value' => '' }, { 'name' => 'Second', 'value' => '' }, { 'name' => 'Third', 'value' => '' }, { 'name' => 'Fourth', 'value' => '' }]
+    uri = URI.parse(CONFIG['sparql_server'] + 'select?output=json')
+
+    query = 'PREFIX foo: <' + CONFIG['ontology_prefix'] + '#>
+             SELECT DISTINCT ?s ?p ?o
+             WHERE {
+               ?s a foo:CompositionTaxonomyTag ;
+                  ?p ?o .
+             } '
+
+    response = Net::HTTP.post_form(uri, 'query' => query)
+
+    render json: parse_results(response.body)
+  end
+
+  def parse_results(data)
+    data = JSON.parse(data)['results']['bindings']
+    results = {}
+
+    data.each do |triple|
+      triple.each { |k, v| v['value'] = v['value'][v['value'].index('#')+1, v['value'].length] }
+
+      results[triple['s']['value']] = results[triple['s']['value']] || {}
+      results[triple['s']['value']][triple['p']['value']] = results[triple['s']['value']][triple['p']['value']] || []
+      results[triple['s']['value']][triple['p']['value']] += [triple['o']['value']]
+    end
+
+    results
   end
 end
