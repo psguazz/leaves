57eaf6dcc68740f67614408d0ee1f9f87ba35d00
Sguazz <psguazza@bwc.org>
2014-10-16 11:25:34 +0300

    Moved WebMock gem to TEST
    Because when it's in development it breaks everything.

--- Gemfile
+++ Gemfile
@@ -6,12 +6,12 @@ group :development, :test do
   gem 'sqlite3'
   gem 'rspec-rails'
   gem 'autotest'
-  gem 'webmock'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   #gem 'capybara', '2.1.0'
+  gem 'webmock'
 end
 
 gem 'sass-rails', '4.0.3'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
